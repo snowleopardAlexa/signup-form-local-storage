1b452e24f1fa09615ac99643a147110b0137a291
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-12 16:55:49 -0500

    add state form info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import {Form, Row, Col, Input, Button, Label, FormGroup} from 'reactstrap';
 
 function App() {
+
+  const [appts, setAppts] = useState([]);
+  const [petName, setPetName] = useState("");
+  const [owner, setOwner] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+
+
 return (
 <div className="pet-appt-form">
 <h2 style={{textAlign: 'center'}}>Pet Appointment Form</h2>
